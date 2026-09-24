55be6b646857a4759c1ec0a5d0d4debad45fecbc
kbibko99-ai <kbibko99@mail.ru>
2026-09-24 19:11:14 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/Happy-Birthday-Marina-">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
